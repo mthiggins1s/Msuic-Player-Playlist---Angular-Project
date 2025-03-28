58097809e288508ff9253ce420561d5faf1d233e
Matthew Higgins <mthiggins1s@semo.edu>
2025-03-27 19:29:03 -0500

    first commit

---
+++ 01-starting-project/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+
+// Decorator; TS feature that adds metadata to the class.
+@Component({
+  selector: 'app-root', // tells Angular what elements it should look for in the HTML file to it can replace it with markup.
+  standalone: true,
+  imports: [HeaderComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+// class named AppComponent; being imported and passed to the bootstrapApplication.
+export class AppComponent {}
